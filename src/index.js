869b23672f6814acf16bb9d5b30bb4bf400c265b
honnmadesu6 <honnmadesu6@gmail.com>
2022-08-18 18:22:51 +0900

    fe

--- src/index.js
+++ src/index.js
@@ -4,8 +4,8 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import Amplify from 'aws-amplify';
-import config from './awsmobile';
-Amplify.configure(config);
+import awsmobile from './aws-exports';
+Amplify.configure(awsmobile);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
