001de7abdf6bbc2a53f7b5630e4aafdf77334bce
honnmadesu6 <honnmadesu6@gmail.com>
2022-08-18 18:33:06 +0900

    added auth

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import Amplify from 'aws-amplify';
 import awsmobile from './aws-exports';
+
 Amplify.configure(awsmobile);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
